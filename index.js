b30280e25eb9d1145c35ebe15a1f56ede1ce89c3
Florian Stabel <f.stabel@gmx.de>
2023-01-16 12:54:18 +0100

    feat: custom setting of passwordlength
    Added a form for custom settings. Adapted generate-pw-button to the form
    (input submit instead of outstanding button) and event-listener to handle
    custom formdata.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import characters from "./data.js"
 import testFunction from "./test.js"
 
-
+const settingsForm = document.querySelector("form")
 
 document.addEventListener('click', (event) => {
     if (event.target.id === "pw-generator-btn") {
-        renderPw()
+        event.preventDefault()
+        const pwSettingsFormData = new FormData(settingsForm)
+        const pwLength = parseInt(pwSettingsFormData.get("pw-setting-length"))
+        renderPw(pwLength)
+
     } else if (event.target.classList.contains("copy-btn")) {
         copyPwToClipboard(event.target.parentElement.textContent.trim())
     } else if (event.target.classList.contains("hide-btn")) {
@@ -15,26 +19,25 @@ document.addEventListener('click', (event) => {
 
 
 
-function renderPw() {
-    document.getElementById("pw-output-one").innerHTML = getPwHtml()
-    document.getElementById("pw-output-two").innerHTML = getPwHtml()
+function renderPw(pwLength) {
+    document.getElementById("pw-output-one").innerHTML = getPwHtml(pwLength)
+    document.getElementById("pw-output-two").innerHTML = getPwHtml(pwLength)
 }
 
 
-function getPwHtml() {
+function getPwHtml(pwLength) {
     return `
-        <span class="pw-string mask" >${generatePw()}</span>
+        <span class="pw-string mask" >${generatePw(pwLength)}</span>
         <i class="fa-regular fa-eye-slash hide-btn"></i>
         <i class="fa-regular fa-copy copy-btn"></i>
     `
 }
 
 
-function generatePw() {
-    const pwArray = new Array(15).fill("0").map((char) => {
+function generatePw(pwLength) {
+    const pwArray = new Array(pwLength).fill("0").map((char) => {
         return characters[Math.floor(Math.random() * characters.length)]
     })
-    console.log(pwArray.join(""))
     return pwArray.join("")
 }
 
@@ -44,6 +47,5 @@ function copyPwToClipboard(string) {
 }
 
 
-
 // testing
-testFunction(1000)
+testFunction(10000)
