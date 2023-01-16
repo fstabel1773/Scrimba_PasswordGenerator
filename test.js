d6d3a2e12324829865c6df8ceb2332b075bc8c77
Florian Stabel <f.stabel@gmx.de>
2023-01-16 13:00:04 +0100

    test: testing random passwordlengths

--- test.js
+++ test.js
@@ -1,25 +1,32 @@
 function testFunction (numOfRuns) {
     let errors = []
     for (let i = 0; i < numOfRuns; i++) {
+        // setting pw-length
+        document.getElementById("pw-setting-length").removeAttribute("value")
+        document.getElementById("pw-setting-length").setAttribute("value", Math.floor(Math.random() * 20))
+        let targetPwLength = parseInt(document.getElementById("pw-setting-length").value)
+        
+        // simulate pw-genaration
         document.getElementById("pw-generator-btn").click()
         const pwOne = document.querySelector("#pw-output-one .pw-string").textContent
         const pwTwo = document.querySelector("#pw-output-two .pw-string").textContent
         
-        
-        if (pwOne.length != 15) {
-            errors.push(new Error(i, pwOne.length, pwOne))
+        // test pw-length
+        if (pwOne.length != targetPwLength) {
+            errors.push(new Error(i, pwOne.length, targetPwLength, pwOne))
         }
-        if (pwTwo.length != 15) {
-            errors.push(new Error(i, pwTwo.length, pwTwo))
+        if (pwTwo.length != targetPwLength) {
+            errors.push(new Error(i, pwTwo.length, targetPwLength, pwTwo))
         }
     }
     console.log(errors)
 }
 
 
-function Error(runNumber, pwLength, pwString) {
+function Error(runNumber, actualPwLength, targetPwLength, pwString) {
     this.runNumber = runNumber;
-    this.pwLength = pwLength;
+    this.actualPwLength = actualPwLength;
+    this.targetPwLength = targetPwLength;
     this.pwString = pwString;
 }
 
